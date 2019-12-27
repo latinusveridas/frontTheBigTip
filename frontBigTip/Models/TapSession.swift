298d5eccd4f313c1851fbc41a8e3da82d3ced7b0
EC2 Default User <sparteclaudius749@gmail.com>
2019-12-27 16:32:17 +0000

    updateCloud9

---
+++ frontBigTip/Models/TapSession.swift
@@ -0,0 +1,23 @@
+//
+//  TapSession.swift
+//  TheBigTip_0.0.0
+//
+//  Created by Quentin Duquesne on 02/01/2020.
+//  Copyright © 2020 Quentin Duquesne. All rights reserved.
+//
+
+/* A tap session is a sequence of tap */
+/* Once the session is finished, the data packet is sent to the API to debit the remaining coins */
+
+import Foundation
+
+isTapSessionRunning: Bool = False
+
+struc TapSession {
+    
+    var lastTap: Tip // LastTap is reset when re-taping before end of countdown 
+    fileprivate var countdown: Double // Represent the time 
+    let timer = Timer()
+    
+    
+}
